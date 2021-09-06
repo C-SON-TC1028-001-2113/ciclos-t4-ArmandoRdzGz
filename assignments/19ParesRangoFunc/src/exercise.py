c0e30d5a31f91d1a6a993b22d03928507244fb47
ArmandoRdzGz <A01254249@itesm.mx>
2021-09-06 23:35:07 +0000

    Act_Pares_Rango

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,6 +1,20 @@
 
 def main():
+    n1 = int(input('Valor 1: ')) 
+    n2 = int(input('Valor 2: '))
 
+    if n1>0 and n2>0 :
+        if n1!=n2:
+            if n1>n2:
+                for numero in range(n2, n1+1):
+                    if numero%2==0 :
+                        print(numero)
+            elif n2>n1 :
+                for numero in range(n1, n2+1):
+                    if numero%2==0 :
+                        print(numero)
+        else:
+            print('No hay pares')
 
 if __name__=='__main__':
     main()
